7acebde5637b88ae802bd0b09cc8220a37608da2
Vikram Kumar <vik@japanvik.net>
2018-03-08 08:30:46 +0900

    new directory structure for neovim

---
+++ neovim_linker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vim/colors ~/.local/share/nvim/site/
+ln -sf ~/dotfiles/.vim/snips ~/.local/share/nvim/site/
+ln -sf ~/dotfiles/.vim/autoload ~/.local/share/nvim/site/
+
